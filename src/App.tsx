2bf0ad107dd7b59025157e6ca9b158043ee9128c
Philip White <philip@philipwhite.dev>
2025-05-19 21:01:19 +0100

    Remove Header to Make Room in Mobile View

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import "./App.css";
 import Game from "./components/Game";
-import Header from "./components/Header";
 
 function App() {
   return (
     <>
       <div className="centre">
         <div className="app">
-          <Header />
           <Game />
         </div>
       </div>
